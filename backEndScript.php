ff61f6b82a54c0dafdc0b713132d0df6cc71a57c
Ivars Rugelis <rugelis.ivars@gmail.com>
2022-12-17 15:57:15 +0200

    Adding data to sql db. Problem with date format.

--- backEndScript.php
+++ backEndScript.php
@@ -1,7 +1,21 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "appointments";
+
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+if(!$connection){
+    die("Connection failed" . $connection->connect_error);
+}
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $date = $_POST['date'];
+
+$sql = "INSERT INTO list (name, email, date)
+VALUES ('$name', '$email', '$date')";
 
 // for email to be actually send the app must run on internet, because from localhost it wont work.
 
@@ -13,7 +27,12 @@ if(!empty($name) && !empty($email)){
         $sender = "From: $email";
 
         if(mail($reciver, $subject, $body, $sender)){
+            if($connection->query($sql)){
             echo "Appointmant made.";
+        }else{
+            echo "Error: " . $sql . "<br>" . $connection->connect_error;
+        }
+        $connection->close();
         }else{
             echo "Sorry faild to make an appointment!";
         }
@@ -23,3 +42,4 @@ if(!empty($name) && !empty($email)){
 }else{
     echo "Fill out all fields!";
 }
+?>
